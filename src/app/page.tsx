a672d87d236ca6ce1e1911146f2da9bed43932ea
Welison Wessling <welison.smi@hotmail.com>
2023-08-07 15:57:41 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+
+      </header>
+      <main>
+        <div>
+          <h1>Calcule o seu IMC</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde (OMS) para calcular o peso ideal de cada pessoa</p>
+        </div>
+      </main>
+    </div>  
+  );
+};
